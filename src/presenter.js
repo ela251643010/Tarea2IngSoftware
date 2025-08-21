44a3c2c6ad4820dfab89a3fc22298c5e6388a39d
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-21 08:28:55 -0400

    feat: saludo por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -5,7 +5,7 @@ const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
-
+/*
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -18,4 +18,12 @@ form.addEventListener("submit", (event) => {
     div.innerHTML = `<p>${multiplicar(firstNumber, secondNumber)}</p>`;
   }
   //div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});*/
+const resultadoDiv = document.querySelector("#resultado-div");
+const formulario = document.querySelector("#saludar-form");
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombreInput = formulario.querySelector("#nombre-input");
+  resultadoDiv.innerHTML = `<p>Hola ${nombreInput.value}</p>`;
 });
