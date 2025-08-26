64fbdcbac2f7a2a0df88b8ad60c5865908d2c374
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-26 09:41:06 -0400

    feat: pruebas division de decimales

--- src/dividir.test.js
+++ src/dividir.test.js
@@ -5,6 +5,9 @@ describe("Dividir", () => {
   it("deberia dividir dos numeros", () => {
     expect(dividir(10, 2)).toEqual(5);
   });
+  it("deberia dividir dos numeros", () => {
+    expect(dividir(9, 2)).toEqual(4.5);
+  });
 });
 
 
