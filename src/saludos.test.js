a44d07c6b1d8064a6c421e560fe14ce04c5f8829
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-26 11:41:38 -0400

    feat: pruebas de saludo en ingles y espanol completas

--- src/saludos.test.js
+++ src/saludos.test.js
@@ -1,6 +1,6 @@
 // tests/saludarGeneroEdad.test.js
 import saludarPorGeneroYEdad from "./saludarGeneroEdad.js";
-import saludarPorHora from "./saludarHora.js";
+import saludarPorHora from "./saludarHoraEidioma.js";
 
 describe("Saludar por género y edad", () => {
   it("debería retornar 'joven' para un masculino menor de 18", () => {
@@ -18,16 +18,30 @@ describe("Saludar por género y edad", () => {
   it("debería retornar 'Señora' para una femenina mayor de 18", () => {
     expect(saludarPorGeneroYEdad(30, "femenino")).toBe("Señora");
   });
-  it("debería retornar un saludo adecuado según la hora del día", () => {
-    const saludo = saludarPorHora();
+
+  it("debería retornar un saludo adecuado en español según la hora del día", () => {
+    const saludo = saludarPorHora("es");
     const horaActual = new Date().getHours();
 
     if (horaActual < 12) {
-      expect(saludo).toBe("¡Buenos días! ");
+      expect(saludo).toBe("¡Buenos días!");
     } else if (horaActual < 18) {
-      expect(saludo).toBe("¡Buenas tardes! ");
+      expect(saludo).toBe("¡Buenas tardes!");
     } else {
-      expect(saludo).toBe("¡Buenas noches! ");
+      expect(saludo).toBe("¡Buenas noches!");
+    }
+  });
+
+  it("should return a proper greeting in English depending on the time of day", () => {
+    const saludo = saludarPorHora("en");
+    const horaActual = new Date().getHours();
+
+    if (horaActual < 12) {
+      expect(saludo).toBe("Good morning!");
+    } else if (horaActual < 18) {
+      expect(saludo).toBe("Good afternoon!");
+    } else {
+      expect(saludo).toBe("Good night!");
     }
   });
 });
