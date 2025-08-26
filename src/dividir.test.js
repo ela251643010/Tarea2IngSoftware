402a46b68f78f2fb579ad225cdf4d004ce3b2b07
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-08-26 09:38:49 -0400

    feat: pruebas de la division

---
+++ src/dividir.test.js
@@ -0,0 +1,10 @@
+import dividir from "./dividir.js";
+import sumar from "./dividir.js";
+
+describe("Dividir", () => {
+  it("deberia dividir dos numeros", () => {
+    expect(dividir(10, 2)).toEqual(5);
+  });
+});
+
+
